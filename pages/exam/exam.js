df14c56fc65f95c8ac30a10a4da26d01e02063f7
杨晓辉 <yangxh108@126.com>
2017-09-29 18:14:00 +0800

    first commit

---
+++ pages/exam/exam.js
@@ -0,0 +1,32 @@
+Page({
+  data:{
+     class_arrow_src:'images/bt_question_link_array_down.png',
+     array:['yangxh','默认班级','+新建班级'],
+     
+     index:0,
+     publisher:['人教版','鲁教版'],
+     publisherId:0,
+     grade:[{name:'人教版',grades:['七年级上','八年级上']},
+     {name:'鲁教版',grades:['鲁教六上','鲁教六下']}],
+     gradeId:0
+  },
+  bindClassChange:function(e){
+    console.log("picker发送事件:",e.detail.value);
+    this.setData({
+      index:e.detail.value
+    })
+  },
+  bindPublisherChange:function(e){
+     console.log("bindPublisherChange发送事件值：",e.detail.value);
+     this.setData({
+       publisherId:e.detail.value,
+       gradeId:0
+     })
+  },
+  bindGradeChange:function(e){
+      console.log("bindGradeChange发送事件值：",e.detail.value);
+      this.setData({
+        gradeId:e.detail.value,
+      })
+  }
+})
