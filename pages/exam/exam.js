9b99d7a77c5b93666d5d30b206d3ef745f2dc551
xavieryang <xavieryang2011@gmail.com>
2017-10-10 18:00:51 +0800

    单元详情页

--- pages/exam/exam.js
+++ pages/exam/exam.js
@@ -1,43 +1,49 @@
-const api=require("../../libraries/data.js")
+const api = require("../../libraries/data.js")
 Page({
-  data:{
-     class_arrow_src:'images/bt_question_link_array_down.png',
-     array:['yangxh','默认班级','+新建班级'],
-     index:0,
-     
-     publisher:['人教版','鲁教版'],
-     publisherId:0,
-     grade:[{name:'人教版',grades:['七年级上','八年级上']},
-     {name:'鲁教版',grades:['鲁教六上','鲁教六下']}],
-     gradeId:0,
+  data: {
+    class_arrow_src: 'images/bt_question_link_array_down.png',
+    array: ['yangxh', '默认班级', '+新建班级'],
+    index: 0,
 
-     unitInfos:{}
+    publisher: ['人教版', '鲁教版'],
+    publisherId: 0,
+    grade: [{ name: '人教版', grades: ['七年级上', '八年级上'] },
+    { name: '鲁教版', grades: ['鲁教六上', '鲁教六下'] }],
+    gradeId: 0,
+
+    unitInfos: {}
   },
-  bindClassChange:function(e){
-    console.log("picker发送事件:",e.detail.value);
+  bindClassChange: function (e) {
+    console.log("picker发送事件:", e.detail.value);
     this.setData({
-      index:e.detail.value
+      index: e.detail.value
     })
   },
-  bindPublisherChange:function(e){
-     console.log("bindPublisherChange发送事件值：",e.detail.value);
-     this.setData({
-       publisherId:e.detail.value,
-       gradeId:0
-     })
+  bindPublisherChange: function (e) {
+    console.log("bindPublisherChange发送事件值：", e.detail.value);
+    this.setData({
+      publisherId: e.detail.value,
+      gradeId: 0
+    })
   },
-  bindGradeChange:function(e){
-      console.log("bindGradeChange发送事件值：",e.detail.value);
-      this.setData({
-        gradeId:e.detail.value,
+  bindGradeChange: function (e) {
+    console.log("bindGradeChange发送事件值：", e.detail.value);
+    this.setData({
+      gradeId: e.detail.value,
+    })
+  },
+  onLoad() {
+    api.getData('getUnitList')
+      .then(d => {
+        this.setData({
+          unitInfos: d.data.unitList
+        })
       })
   },
-  onLoad(){
-      api.getData('getUnitList')
-          .then(d=>{
-            this.setData({
-                unitInfos: d.data.unitList
-              })
-          })
+  showDetail: function (event) {
+    console.log(event.currentTarget.dataset.unitinfo);
+    wx.navigateTo({
+      url: '../examDetail/examDetail?unitID=' + event.currentTarget.dataset.unitinfo
+    })
   }
 })
