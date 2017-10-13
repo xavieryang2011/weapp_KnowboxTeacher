aab4879b4839589f2d4ea50d6902d9352ef91ebe
xavieryang <xavieryang2011@gmail.com>
2017-10-13 10:09:07 +0800

    完成发布

--- pages/deploy/deploy.js
+++ pages/deploy/deploy.js
@@ -7,6 +7,7 @@ Page({
   data: {
       unitName:'Unit1',
       time:'00:00',
+      durationTime:'00:00',
       classInfo:{}
   },
 
@@ -19,7 +20,8 @@ Page({
     var mins=myDate.getMinutes()<10?'0'+myDate.getMinutes():myDate.getMinutes();
      this.setData({
        unitName:options.unitName,
-       time:hours+':'+mins
+       time:hours+':'+mins,
+       durationTime:hours+':'+mins
      })
      wx.setNavigationBarTitle({
        title: '发布测验',
@@ -86,10 +88,33 @@ Page({
       time:e.detail.value
     })
   },
+  bindDurationTimeChange:function(e){
+    this.setData({
+      durationTime:e.detail.value
+    })
+  },
   switchBindChange:function(e){
 
   },
   checkboxchanged:function(e){
     console.log(e);
+  },
+  deploy:function(e){
+    // wx.navigateTo({
+    //   url: '../finish/finish',
+    // })
+   wx.showModal({
+     title: '提示',
+     content: '发布测验成功',
+     showCancel:false,
+     success:function(res){
+       if(res.confirm){
+         console.log(res.confirm);
+        wx.switchTab({
+          url: '../exam/exam',
+        })
+       }
+     }
+   })
   }
 })
